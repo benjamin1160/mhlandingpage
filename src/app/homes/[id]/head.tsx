6d6f424c0c18f52374aba230df3931fb0cb337b8
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-07-13 15:57:20 -0400

    Fix type imports and typings

--- src/app/homes/[id]/head.tsx
+++ src/app/homes/[id]/head.tsx
@@ -1,11 +1,7 @@
 // src/app/homes/[id]/head.tsx
 import type { Metadata } from "next";
 
-export default function Head({
-  params,
-}: {
-  params: { id: string };
-}): Metadata {
+export default function Head({ params }: { params: { id: string } }): Metadata {
   return {
     title: `Dream Home #${params.id}`,
     description: `Customize and preview your manufactured home #${params.id}.`,
