37d8b29dbfbbe057905f897a3f5b4bb63aad8d90
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-07-14 00:17:59 -0400

    add prisma client stub

---
+++ src/prisma-stub.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module "@prisma/client" {
+  class PrismaClient {
+    [key: string]: any
+    constructor()
+    $disconnect(): Promise<void>
+    $connect(): Promise<void>
+  }
+  export { PrismaClient }
+}
